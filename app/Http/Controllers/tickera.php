48977b091938fc64dc671412e44a9a9620b68e59
Alvaro Ospino <34920387+alvaritojose2712@users.noreply.github.com>
2025-04-03 19:54:45 -0400

    fdfd

--- app/Http/Controllers/tickera.php
+++ app/Http/Controllers/tickera.php
@@ -529,7 +529,7 @@ class tickera extends Controller
                         "estado"=>false,
                     ]); */
                 }
-                array_push($factura,"i05Vendedor: ".($pedido->vendedor->usuario)." - ".$id."\n" );
+                array_push($factura,"i05Caja: ".($pedido->vendedor->usuario)." - ".$id."\n" );
 
 
 
